ff23facbd9eb7e2861174a9a2839fb035d5afd62
moen <moenr49@gmail.com>
2025-06-28 09:12:01 +0300

    Implement code changes to enhance functionality and improve performance

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+  const button = document.getElementById('button'); // use the correct ID!
+  button.addEventListener('click', function () {
+    alert('I was clicked!');
+  });
 }
+
+// If you're running tests in Node, export it:
+module.exports = { addingEventListener };
